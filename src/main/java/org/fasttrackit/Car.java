e3c86fde5371fbda44b3f109789f27beb8ddf52b
Andrei Bundas <andreibundas@gmail.com>
2020-06-10 13:46:26 +0300

    added Car class

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,20 @@
+package org.fasttrackit;
+
+public class Car {
+
+    // JAVA un limbaj de programare - Strong-Typed - adica trebuie sa specificam tipul datelor ( String, double, boolean etc)
+    // spre deosebire, Java Script este un limbaj de programare weak-typed, in sensul in care
+    // se pot declara variable la modul general: var  ( fara nominalizare )
+
+    String make;  // marca
+    String model;  // model
+    String color;
+    double mileage;  // consumul mediu
+    double fuelLevel;
+    double maxSpeed;
+    double totalTravelDistance;
+    boolean damaged;
+    int doorCount;
+
+
+}
